bf4bb0705478f83b6b9ebbc1652b6777a05f719d
xzdunno <dudnikov.anton1306@yandex.ru>
2021-11-09 23:55:54 +0300

    some changes

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.weather
 
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weather.databinding.ActivityMainBinding
@@ -14,15 +12,8 @@ import okhttp3.Call
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.json.JSONObject
-import java.io.BufferedReader
 import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
-import java.net.URL
-import javax.security.auth.callback.Callback
+
 
 
 class MainActivity : AppCompatActivity() {
